a2ff805a2a3f0dc3a9e6972d62c70f285d759e41
Matt Burke <git@mattburkedev.com>
2019-12-11 15:45:59 -0600

    Make server accessible from remote machines

--- keg-server/src/server.ts
+++ keg-server/src/server.ts
@@ -14,7 +14,7 @@ app.use(errorHandler());
 /**
  * Start Express server.
  */
-const server = app.listen(app.get('port'), () => {
+const server = app.listen(app.get('port'), '0.0.0.0', () => {
   logger.info('App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env'));
   // console.log('  App is running at http://localhost:%d in %s mode', app.get('port'), app.get('env'));
   // console.log('  Press CTRL-C to stop\n');
