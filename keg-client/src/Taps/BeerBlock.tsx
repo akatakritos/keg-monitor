9913f5e385460d4e59e4df30b44c06c6e15fd44b
Nick Hotalling <nick.hotalling@artisantechgroup.com>
2019-12-12 12:48:13 -0600

    Changed voting back to buttons, removed from chips

--- keg-client/src/Taps/BeerBlock.tsx
+++ keg-client/src/Taps/BeerBlock.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import { Beer } from '../ServerModels';
+import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Chip from '@material-ui/core/Chip';
@@ -19,19 +21,35 @@ import './BeerBlock.css';
 const useStyles = makeStyles(theme => ({
   card: {},
   button: {
-    margin: theme.spacing(1),
+    margin: theme.spacing(0.25),
+  },
+  upVoteButton: {
+    backgroundColor: '#40BF40',
+    '&:hover': {
+      background: '#2E892E',
+    },
+  },
+  downVoteButton: {
+    backgroundColor: '#BF4040',
+    '&:hover': {
+      background: '#913131',
+    },
   },
   leftIcon: {
     marginRight: theme.spacing(1),
-  },
-  rightIcon: {
-    marginLeft: theme.spacing(1),
   },
   iconSmall: {
     fontSize: 20,
   },
   centered: {
     textAlign: 'center',
+  },
+  beerName: {
+    flexGrow: 1,
+  },
+  chip: {
+    marginRight: theme.spacing(0.5),
+    marginBottom: theme.spacing(0.5),
   },
 }));
 
@@ -48,6 +66,8 @@ export function BeerBlock(props: BeerProps) {
   const tapped = parseISO(props.tapped);
   const age = differenceInDays(today, tapped);
   const beer = props.beer;
+  const upVotes = beer.upvotes ? beer.upvotes : 0;
+  const downVotes = beer.downvotes ? beer.downvotes : 0;
   const [isDialogOpen, setDialogOpen] = React.useState(false);
 
   const handleDialogOpen = () => {
@@ -70,34 +90,29 @@ export function BeerBlock(props: BeerProps) {
       </div>
       <DetailDialog beer={beer} isOpen={isDialogOpen} onClose={handleDialogClose} />
       <CardContent>
-        <Typography variant="h5" component="h2">
-          {beer.name}
-        </Typography>
+        <Box display="flex" flexWrap="nowrap">
+          <Typography variant="h5" component="h2" className={classes.beerName}>
+            {beer.name}
+          </Typography>
+          <VoteButtons onClick={onVoteButtonClick} upVotes={upVotes} downVotes={downVotes} />
+        </Box>
         <Typography gutterBottom variant="body2" component="p">
           {beer.brewer}
         </Typography>
-        <div>
-          {beer.style ? <Chip icon={<Icon>local_drink</Icon>} label={beer.style} /> : null}
+        <Box>
+          {beer.style ? <Chip icon={<Icon>local_drink</Icon>} label={beer.style} className={classes.chip} /> : null}
           {beer.abv ? (
-            <Chip icon={<Icon>sentiment_very_satisfied</Icon>} label={beer.abv.toFixed(1) + '% ABV'} />
+            <Chip
+              icon={<Icon>sentiment_very_satisfied</Icon>}
+              label={beer.abv.toFixed(1) + '% ABV'}
+              className={classes.chip}
+            />
           ) : null}
-          {beer.bitterness ? <Chip icon={<Icon>mood_bad</Icon>} label={beer.bitterness.toFixed(1) + ' IBU'} /> : null}
+          {beer.bitterness ? (
+            <Chip icon={<Icon>mood_bad</Icon>} label={beer.bitterness.toFixed(1) + ' IBU'} className={classes.chip} />
+          ) : null}
           <Chip icon={<Icon>today</Icon>} label={age + ' days ago'} />
-          <Chip
-            icon={<Icon>thumb_up</Icon>}
-            label={beer.upvotes ? beer.upvotes : 0}
-            onClick={() => onVoteButtonClick(true)}
-            style={{ backgroundColor: '#40BF40' }}
-            color="secondary"
-          />
-          <Chip
-            icon={<Icon>thumb_down</Icon>}
-            label={beer.downvotes ? beer.downvotes : 0}
-            onClick={() => onVoteButtonClick(false)}
-            style={{ backgroundColor: '#BF4040' }}
-            color="secondary"
-          />
-        </div>
+        </Box>
       </CardContent>
     </Card>
   );
@@ -146,30 +161,45 @@ function DetailDialog(props: DetailDialogProps) {
 
 export interface VoteButtonsProps {
   onClick: (isUpVote: boolean) => void;
+  upVotes: number;
+  downVotes: number;
 }
 
-// Changed Chips to be clickable due to real-estate restrictions
-// function VoteButtons(props: VoteButtonsProps) {
-//   const classes = useStyles();
+function VoteButtons(props: VoteButtonsProps) {
+  const classes = useStyles();
 
-//   const handleUpVote = () => {
-//     props.onClick(true);
-//   };
+  const handleUpVote = () => {
+    props.onClick(true);
+  };
 
-//   const handleDownVote = () => {
-//     props.onClick(false);
-//   };
+  const handleDownVote = () => {
+    props.onClick(false);
+  };
 
-//   return (
-//     <React.Fragment>
-//       <Button size="small" color="primary" variant="contained" className={classes.button} onClick={handleUpVote}>
-//         <Icon className={clsx(classes.leftIcon, classes.iconSmall)}>thumb_up</Icon>
-//         Great
-//       </Button>
-//       <Button size="small" color="secondary" variant="contained" className={classes.button} onClick={handleDownVote}>
-//         <Icon className={clsx(classes.leftIcon, classes.iconSmall)}>thumb_down</Icon>
-//         Meh
-//       </Button>
-//     </React.Fragment>
-//   );
-// }
+  return (
+    <React.Fragment>
+      <ButtonGroup>
+        <Button
+          size="small"
+          color="primary"
+          variant="contained"
+          className={clsx(classes.button, classes.upVoteButton)}
+          onClick={handleUpVote}
+        >
+          <Icon className={clsx(classes.leftIcon, classes.iconSmall)}>thumb_up</Icon>
+          {props.upVotes}
+        </Button>
+        <Button
+          size="small"
+          color="secondary"
+          variant="contained"
+          className={clsx(classes.button, classes.downVoteButton)}
+          onClick={handleDownVote}
+        >
+          <Icon className={clsx(classes.leftIcon, classes.iconSmall)}>thumb_down</Icon>
+          {props.downVotes}
+        </Button>
+      </ButtonGroup>
+    </React.Fragment>
+  );
+}
